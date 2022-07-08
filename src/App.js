095b0ad85f39d192fae809779e8470abd839d0e0
shahadat2772 <shahadat2772@gmailcom>
2022-07-08 09:34:53 +0600

    heelloo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>HELLO THERE </h2>
+      <h2>HELLO THERE</h2>
+      <h2>HELLO from branch1</h2>
+      <h2>HELLO from branch1 (second)</h2>
       <h2>THIS IS A IMPORTANT CODE</h2>
     </div>
   );
